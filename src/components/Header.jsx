6ec735e39fa412ae92c337821d48b0ec32635564
Ian Mwambuki <merlnadika@gmail.com>
2026-04-26 21:14:03 +0300

    Add Header component with navigation links

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Choche Flani</h1>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
